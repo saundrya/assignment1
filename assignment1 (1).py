aee9bc157103c807902543acc14a6445b27f56a3
saundrya <82432613+saundrya@users.noreply.github.com>
2021-04-23 21:48:51 -0700

    Add files via upload

---
+++ assignment1 (1).py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+for i in range(2000,3201):
+   if (i%7==0) and (i%5!=0):
+      print(i,end=", ")
+
+
+
+# In[9]:
+
+
+firstname=input("input your first name:-")
+lastname=input("input your last name:-")
+print(lastname+" "+firstname)
+
+
+# In[13]:
+
+
+d=12
+r=d/2
+pie=3.14
+v=4/3*pie*r**3
+print("the volume of sphere:-",v)
+
